57660b400420194ae9ea69226539ad56c915e153
vinayan mohan <vinayanmohan87@gmail.com>
2026-06-17 11:24:36 +0530

    Add files via upload

---
+++ four.py
@@ -0,0 +1,59 @@
+a = 7
+b = 9
+print (a == b)
+print (a != b)
+print (a > b)
+print (a < b)
+print (a >= b)
+print (a <= b)
+
+age = 11
+if age >= 18:
+    print ("eligible")
+else:    print ("not eligible")
+
+num = 12346
+if num % 2 == 0:
+    print ("even")
+else:
+    print ("odd")
+
+num = 1050
+if num % 5 == 0 :
+    print ("multiple of 5")
+
+
+
+
+    mark = 75
+if mark >= 70:
+    print ("B+")
+elif mark >= 60:
+    print ("B")
+elif mark >= 50:
+    print ("C")
+elif mark >= 40:
+    print ("D")
+else:    print ("fail")
+
+
+num = 10
+if num > 0:
+    print ("positive")
+elif num < 0:
+    print ("negative")
+else:
+    print ("zero")
+
+    a = 10
+    b = 11
+    c = 100
+if a > b and a > c:
+    print ("largest num is a")
+elif b > a and b > c:
+    print ("largest num is b")
+else:
+    print ("largest num is c")
+
+
+
