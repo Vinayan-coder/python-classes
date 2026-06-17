357c925ba1dca5a8eccd491a3e02a6d5d2f3087e
vinayan mohan <vinayanmohan87@gmail.com>
2026-06-17 11:45:23 +0530

    Add files via upload

---
+++ three.py
@@ -0,0 +1,17 @@
+a = 6
+print (type(a))
+name = 'mohan'
+print (type(name))
+a = 0
+print (a)
+print (type(a))
+a = 9
+b = 4
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+print (a ** b)
+print (a // b)
+print (a % b)
+
---
+++ two.py
@@ -0,0 +1,31 @@
+# variables
+# variable = value
+age = 5
+AGE = 15
+Age = 20
+print (age)
+print (AGE)
+print (Age)
+
+# print (age)
+a = 3
+b = 7
+# swap 2 numbers
+a = 'green'
+b = 'red'
+c = a
+a = b
+b = c
+
+print (a)
+print (b)
+print (c)
+
+a = 5
+b = 7
+a = a + b
+b = a - b
+a = a - b
+
+print (a)
+print (b)
