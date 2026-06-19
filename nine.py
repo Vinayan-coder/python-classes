9c15e23cb65690ba3707bbefd881c61fe3d62bec
vinayan mohan <vinayanmohan87@gmail.com>
2026-06-19 13:13:38 +0530

    Add files via upload

---
+++ nine.py
@@ -0,0 +1,29 @@
+
+
+a = [11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+a.append(30)
+print(a)
+
+a.append(31)
+print(a)
+
+extend = [18,19,20]
+a.extend(['mohan'])
+print(a)
+
+insert = [32,33,34,35]
+a.insert(0,32)
+print(a)
+
+a.remove(32)
+print(a)
+
+pop = a.pop(0)
+print(a)    
+
+tuple = (11,12,13,14,15,16,17,18,19,20,"mohan")
+print(tuple)
+
+
+
+
---
+++ ten.py
@@ -0,0 +1,18 @@
+a = [11,12,13,14,15]
+b = "mohan"
+c = (100,200,300,400,500)
+for i in a:
+    print(i)
+for i in b:
+    print(i)
+for i in c:
+    print(i)
+
+
+a = 'mohan'
+print(len(a))
+
+fruits = ['apple', 'banana', 'orange', 'grapes']
+for i in fruits:
+for i in range(0,(fruits)):
+    print(i)
