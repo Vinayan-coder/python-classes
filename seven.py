4be6dba2c204888beecfab58cbd50f44cb581d87
vinayan mohan <vinayanmohan87@gmail.com>
2026-06-19 12:12:09 +0530

    Add files via upload

---
+++ seven.py
@@ -0,0 +1,14 @@
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print (data[0])
+print (data[4])
+print (data[::2])
+print (data[1:4])
+print (data[::-1])
+print (data[0:9:2])
+
+
+data = "mohan das gandhi"
+print(data[1:4])
+print(data[:-1])
+print(data[::-1])
+
