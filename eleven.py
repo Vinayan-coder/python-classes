d07fca77b80960f90b5764890964faa460b92ffa
vinayan mohan <vinayanmohan87@gmail.com>
2026-06-22 12:39:45 +0530

    Add files via upload

---
+++ eleven.py
@@ -0,0 +1,16 @@
+ # find vowels and their positions in a string
+a = "vinayan"
+for i in range(len(a)):
+    if a[i] in 'aeiou':
+        print(a[i],i)
+
+name = input ("enter your name")
+for i in range(len(name)):
+    print(i,name[i])
+if name[i] == 'a' or name[i] == 'e' or name[i] == 'i' or name[i] == 'o' or name[i] == 'u':
+    print(i,name[i])
+
+
+
+
+
