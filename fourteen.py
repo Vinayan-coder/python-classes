db90210225c66a1bbf4c11ea63da2b579e83b065
vinayan mohan <vinayanmohan87@gmail.com>
2026-06-23 13:21:47 +0530

    Add files via upload

---
+++ fourteen.py
@@ -0,0 +1,17 @@
+a = 'hello my name is kumar'
+b = a.split()
+print(b)
+
+a = 'hello my name is kumar'
+b = ""
+c = []
+for i in a:
+    if i!=" ":
+        b = b +1
+    else:
+    c = a.append(i)
+    b = ""
+c.append(i)
+print(c)
+
+
