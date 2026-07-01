138f311906295a64f25a917e7d73528d22fb46c0
vinayan mohan <vinayanmohan87@gmail.com>
2026-07-01 12:51:00 +0530

    Add files via upload

---
+++ sixteen.py
@@ -0,0 +1,55 @@
+# higher order functioons
+# when a fuction gets afunction as its argument
+# when a function returns a function
+
+#recursion
+# def hello():
+#     print("mohan")
+#     retur hello()
+
+
+# def counttozero(n):#stub
+#         print(n)
+#         if n == 0:
+#             return 0
+#         return counttozero(n-1)
+# counttozero(10)
+
+
+# def counttozero(n):#stub
+#         print(n)
+#         if n == 0:
+#             return 
+#         return counttozero(n-1)
+# counttozero(10)
+
+
+# def sum(n): #stub
+#     print(n)
+#     if n == 0:
+#         return 0
+#     return n + sum(n-1) # 10 + 9(10-1), 9 + 8(9-1),......
+# sum(10)
+
+# def factorial (n): #stub
+#     print()
+#     if n 
+
+
+# name = "vinayan"  #global scope 
+# def myname():
+#     name = "vishaw"
+#     def nickname(): # local scope
+#         name = "mohan"
+#         print(name)
+# print(name)
+
+
+
+# modules
+
+name = "das"
+def add2(a,b):
+    return a+b
+
+
